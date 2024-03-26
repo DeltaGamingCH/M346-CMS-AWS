db10caaeb037046bdfff689592435f913ffe0191
FclaudeM <106908021+FclaudeM@users.noreply.github.com>
2024-03-26 11:09:58 +0100

    Update start.sh

--- Joomla/start.sh
+++ Joomla/start.sh
@@ -1,14 +1,14 @@
 aws ec2 create-key-pair --key-name aws-gbs-cli --key-type rsa --query 'KeyMaterial' --output text > ~/.ssh/aws-gbs-cli.pem
 
-Security group erstellen
+# Security group erstellen
 echo "Creating Security Groups"
 aws ec2 create-security-group --group-name gbs-sec-group --description "Joomla-Setup"
 aws ec2 authorize-security-group-ingress --group-name gbs-sec-group --protocol all --port 80 --cidr 0.0.0.0/0
 echo "creating EC2 Instance"
-EC2 Erzeugen
+# EC2 Erzeugen
 <s-cli --security-groups gbs-sec-group --iam-instance-profile Name=LabInstanceProfile --user-data file://user-data.txt --query 'Ins>
 
-EC2_INSANCE_ID
+# EC2_INSANCE_ID
 EC2_PUBLIC_IP=$(aws ec2 describe-instances --filters Name=instance-id,Values=$EC2_INSTANCE_ID --query 'Reservations[].Instances[]>
 
 echo "Please wait until Joomla is installed."
